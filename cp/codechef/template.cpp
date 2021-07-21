023ff0cc8ff46cb7aa15d52df5fd496f2e1d017d
Puja Arkal <apuja@cdac.in>
2021-07-21 17:59:50 +0530

    warmup

---
+++ cp/codechef/template.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define gc getchar_unlocked
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;i<n;i++)
+#define ll long long
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%I64d",&x)
+#define ss(s)	scanf("%s",s)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define clr(x) memset(x, 0, sizeof(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pll;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pll>		vpll;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+const int mod = 1000000007;
+const int N = 2e5;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        //Your code here.
+    }
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
